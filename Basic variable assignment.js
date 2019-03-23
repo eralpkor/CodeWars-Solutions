f4ef3ee1c2e9e0bdcab3c358abaaeaee6426e5b9
eralpkor <eralp.ny@gmail.com>
2019-03-23 10:29:03 -0400

    more solutions

---
+++ Basic variable assignment.js
@@ -0,0 +1,7 @@
+/* Basic variable assignment
+https://www.codewars.com/kata/basic-variable-assignment/train/javascript
+This code should store "codewa.rs" as a variable called "name" but it's not working. Can you figure out why? */
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
