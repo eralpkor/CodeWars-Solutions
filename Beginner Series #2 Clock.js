2d4b9f5982aeaea48b4cb99ee8be8bb4dc45b838
eralpkor <eralp.ny@gmail.com>
2019-03-25 10:36:40 -0400

    more solutions

--- Beginner Series #2 Clock.js
+++ Beginner Series #2 Clock.js
@@ -9,6 +9,4 @@ Your task is to make 'Past' function which returns time converted to miliseconds
 past(0, 1, 1) == 61000
 Note! h, m and s will be only Natural numbers! */
 
-}
-
 var past = (h, m, s) => ((h * 3600) + (m * 60) + s) * 1000;
